7464cd17f4c8df787b576881d8cfccae1f4961cb
Joseph B <100744018+jobloom79@users.noreply.github.com>
2022-03-27 21:12:48 -0400

    3.2.11

--- Python_practice.py
+++ Python_practice.py
@@ -17,3 +17,15 @@ for county, voters in counties_dict.items():
 
 for county, voters in counties_dict.items():
     print (f"{county} county has {voters} registered voters.")
+
+candidates_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidates_votes:,} number of votes. "
+    f" The total number of votes in the election was {total_votes:,}. "
+    f" You received {candidates_votes / total_votes * 100:.2f}% of the total votes")
+
+print(message_to_candidate)
+
+for county, voters in counties_dict.items():
+    print (f"{county} county has {voters:,} registered voters.")
