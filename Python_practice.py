6e32c8335793d4e68c6ee1faa7ee677fbd7345b4
Joseph B <100744018+jobloom79@users.noreply.github.com>
2022-03-27 20:12:55 -0400

    multiple python practice updates

--- Python_practice.py
+++ Python_practice.py
@@ -1,1 +1,19 @@
-print("Hellow World")
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+for county in counties_dict:
+    print(county)
+
+for county in counties_dict.keys():
+    print(county)
+
+for voters in counties_dict.values():
+    print(voters)
+
+for county in counties_dict:
+    print(counties_dict.get(county))
+
+for county, voters in counties_dict.items():
+    print (county + " has " + str(voters) + " registered voters.")
+
+for county, voters in counties_dict.items():
+    print (f"{county} county has {voters} registered voters.")
