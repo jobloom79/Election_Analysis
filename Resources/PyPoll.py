e3c538aaed97e363d4b6ce2d75af731966ee700a
Joseph B <100744018+jobloom79@users.noreply.github.com>
2022-04-02 18:59:47 -0400

    Update PyPoll.py

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -12,7 +12,9 @@
 # Print the present line.
 #print("The time right now is ", now)
 
-#import csv
+#Add our Dependencies
+import csv
+import os
 #dir(csv)
 #import numbers
 
@@ -24,8 +26,6 @@
 #with open(file_to_load) as election_data:
 #   print(election_data)
 
-import os
-from tkinter import N
 #file_to_load = os.path.join ("Resources","election_results.csv")
 #with open(file_to_load) as election_data:
     #print(election_data)
@@ -42,13 +42,27 @@ from tkinter import N
 #Close the file
 #outfile.close()
 
-#Create a variable to a direct or indirect path
-file_to_save = os.path.join("analysis","election_analysis.txt")
-#use the with statement and open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+
+# #use the with statement and open the file as a text file.
+#with open(file_to_save, "w") as txt_file:
     #write some data to the file.
     #txt_file.write("Hello again")
-    txt_file.write("Counties in the Election")
-    txt_file.write("\n---------------------------")
-    txt_file.write("\nArapahoe\nDenver\nJefferson")
+    #txt_file.write("Counties in the Election")
+    #txt_file.write("\n---------------------------")
+    #txt_file.write("\nArapahoe\nDenver\nJefferson")
+
+#Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources","election_results.csv")
+#Assign a variable to save a file to a path
+file_to_save = os.path.join("analysis","election_analysis.txt")
+#Open the election results and read the file
+with open(file_to_load) as election_data:
+    #Read the file object with the reader function
+    file_reader = csv.reader(election_data)
+    #Print each row in the csv file
+    #for row in file_reader:
+        #print(row)
+    headers = next(file_reader)
+    print(headers)
+
     
