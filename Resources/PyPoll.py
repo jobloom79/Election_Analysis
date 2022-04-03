f65caac9113185e4bfd545dc46726ef23df8520d
Joseph B <100744018+jobloom79@users.noreply.github.com>
2022-04-02 21:28:36 -0400

    3.5.1

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -55,13 +55,26 @@ import os
 file_to_load = os.path.join("Resources","election_results.csv")
 #Assign a variable to save a file to a path
 file_to_save = os.path.join("analysis","election_analysis.txt")
+
+#Add the total vote counter
+total_votes = 0
+
 #Open the election results and read the file
 with open(file_to_load) as election_data:
+    
     #Read the file object with the reader function
     file_reader = csv.reader(election_data)
+    
+    #Read the header row.
+    headers = next(file_reader)
+    
     #Print each row in the csv file
-    #for row in file_reader:
-        #print(row)
+    for row in file_reader:
+        #Increment total votes by 1 after for loop
+        total_votes += 1
+        #print out total votes
+print(total_votes)
     
-
-    
+#file_to_test = os.path.join("analysis","election_test.txt")
+#with open(file_to_test, "w") as txt_file:
+    #txt_file.write("Hello for the 3rd time")
